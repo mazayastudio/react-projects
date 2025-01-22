ef2dc59123a3997ff0c8211b1a03c8bc7617e493
sayudha <mazaya.studio@outlook.com>
2025-01-22 09:42:01 +0700

    Implement pizza menu display with conditional rendering

--- 03-Pizza-Menu/src/index.js
+++ 03-Pizza-Menu/src/index.js
@@ -49,11 +49,41 @@ const pizzaData = [
 
 function App() {
 	const style = { color: 'red', fontSize: '48px', textTransform: 'uppercase' };
+	const pizzas = pizzaData;
+	const numPizzas = pizzas.length;
 	return (
 		<div className='container'>
 			<header className='header'>
 				<h1 style={style}>Fast React Pizza Co.</h1>
 			</header>
+
+			<main className='menu'>
+				<h2>Our menu</h2>
+
+				{numPizzas > 0 ? (
+					<>
+						<p>
+							Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all
+							delicious
+						</p>
+						.
+						<ul className='pizzas'>
+							{pizzas.map((pizza) => (
+								<li>
+									<img src={pizza.photoName} alt={pizza.name} />
+									<div>
+										<h3>{pizza.name}</h3>
+										<p>{pizza.ingredients}</p>
+										<span>{pizza.soldOut ? 'SOLD OUT' : pizza.price}</span>
+									</div>
+								</li>
+							))}
+						</ul>
+					</>
+				) : (
+					<p>We're still working on our menu. Please come back later 🙂</p>
+				)}
+			</main>
 		</div>
 	);
 }
