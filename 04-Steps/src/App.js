65dafcae3400630d47234a0b861f4a9003354a35
sayudha <mazaya.studio@outlook.com>
2025-01-22 10:45:33 +0700

    Add step navigation with messages and buttons in App component

--- 04-Steps/src/App.js
+++ 04-Steps/src/App.js
@@ -1,7 +1,48 @@
-import './App.css';
+import { useState } from 'react';
+
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
 function App() {
+	return <Steps />;
+}
 
+function Steps() {
+	const [step, setStep] = useState(1);
+
+	function handlePrevious() {
+		setStep((currStep) => (currStep > 1 ? currStep - 1 : currStep));
+	}
+
+	function handleNext() {
+		setStep((currStep) => (currStep <= 3 ? currStep + 1 : currStep));
+	}
+	return (
+		<div>
+			<button className='close'>&times;</button>
+
+			<div className='steps'>
+				<div className='numbers'>
+					<div className={step >= 1 ? 'active' : ''}>1</div>
+					<div className={step >= 2 ? 'active' : ''}>2</div>
+					<div className={step >= 3 ? 'active' : ''}>3</div>
+				</div>
+
+				<div className='message'>
+					<h3>Step: {step}</h3>
+					<p>{messages[step - 1]}</p>
+				</div>
+
+				<div className='buttons'>
+					<button style={{ backgroundColor: '#7950f2', textColor: '#fff' }} onClick={handlePrevious}>
+						<span>👈</span> Previous
+					</button>
+					<button style={{ backgroundColor: '#7950f2', textColor: '#fff' }} onClick={handleNext}>
+						Next <span>👉</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
