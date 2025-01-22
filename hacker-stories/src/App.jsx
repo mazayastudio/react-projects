e71194a5065cad376312957c0d0e5fcbe5f334a8
sayudha <mazaya.studio@outlook.com>
2025-01-22 16:50:21 +0700

    Refactor List component to pass individual item properties to Item component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -50,12 +50,19 @@ const Search = ({ searchTerm, onSearch }) => {
 const List = ({ list }) => (
 	<ul>
 		{list.map((item) => (
-			<Item key={item.objectID} item={item} />
+			<Item
+				key={item.objectID}
+				title={item.title}
+				url={item.url}
+				author={item.author}
+				num_comments={item.num_comments}
+				points={item.points}
+			/>
 		))}
 	</ul>
 );
 
-const Item = ({ item: { title, url, author, num_comments, points } }) => (
+const Item = ({ title, url, author, num_comments, points }) => (
 	<li>
 		<span>
 			<a href={url}>{title}</a>
