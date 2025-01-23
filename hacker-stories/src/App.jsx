59227050058ee373aa3e390228ac2ee47e4d4078
sayudha <mazaya.studio@outlook.com>
2025-01-23 15:07:12 +0700

    Add Prettier configuration and update code formatting
    Introduced a `.prettierrc`

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,67 +2,78 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 const useStorageState = (key, initialState) => {
-	const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
 
-	useEffect(() => {
-		localStorage.setItem(key, value);
-	}, [value, key]);
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-	return [value, setValue];
+  return [value, setValue];
 };
 
 const App = () => {
-	const stories = [
-		{
-			title: 'React',
-			url: 'https://reactjs.org/',
-			author: 'Jordan Walke',
-			num_comments: 3,
-			points: 4,
-			objectID: 0,
-		},
-		{
-			title: 'Redux',
-			url: 'https://redux.js.org/',
-			author: 'Dan Abramov, Andrew Clark',
-			num_comments: 2,
-			points: 5,
-			objectID: 1,
-		},
-	];
+  const stories = [
+    {
+      title       : 'React',
+      url         : 'https://reactjs.org/',
+      author      : 'Jordan Walke',
+      num_comments: 3,
+      points      : 4,
+      objectID    : 0,
+    },
+    {
+      title       : 'Redux',
+      url         : 'https://redux.js.org/',
+      author      : 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points      : 5,
+      objectID    : 1,
+    },
+  ];
 
-	// const [searchTerm, setSearchTerm] = useState(localStorage.getItem('search') || 'React');
-	const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  // const [searchTerm, setSearchTerm] = useState(localStorage.getItem('search') || 'React');
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
-	// useEffect(() => {
-	// 	localStorage.setItem('search', searchTerm);
-	// }, [searchTerm]);
+  // useEffect(() => {
+  // 	localStorage.setItem('search', searchTerm);
+  // }, [searchTerm]);
 
-	const handleSearch = (event) => {
-		setSearchTerm(event.target.value);
-	};
-	const handleChange = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleChange = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
-	return (
-		<div>
-			<h1>My Hacker Stories</h1>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-			<InputWithLabel id={'search'} label={'Search: '} value={searchTerm} onInputChange={handleSearch} />
+      <InputWithLabel
+        id='search'
+        type='text'
+        label='Search: '
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
-			{/* <Search searchTerm={searchTerm} onSearch={handleSearch} /> */}
+      {/* <Search searchTerm={searchTerm} onSearch={handleSearch} /> */}
 
-			<hr />
+      <hr />
 
-			<List list={handleChange} />
-		</div>
-	);
+      <List list={handleChange} />
+    </div>
+  );
 };
 
-const InputWithLabel = ({ id, label, value, onInputChange }) => (
-	<>
-		<label htmlFor={id}>{label}</label>
-		<input id={id} type='text' value={value} onChange={onInputChange} />
-	</>
+const InputWithLabel = ({ id, type, label, value, onInputChange }) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    <input
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
+    />
+  </>
 );
 
 // const Search = ({ searchTerm, onSearch }) => {
@@ -76,22 +87,22 @@ const InputWithLabel = ({ id, label, value, onInputChange }) => (
 // };
 
 const List = ({ list }) => (
-	<ul>
-		{list.map(({ objectID, ...item }) => (
-			<Item key={objectID} {...item} />
-		))}
-	</ul>
+  <ul>
+    {list.map(({ objectID, ...item }) => (
+      <Item key={objectID} {...item} />
+    ))}
+  </ul>
 );
 
 const Item = ({ title, url, author, num_comments, points }) => (
-	<li>
-		<span>
-			<a href={url}>{title}</a>
-		</span>
-		<span>{author}</span>
-		<span>{num_comments}</span>
-		<span>{points}</span>
-	</li>
+  <li>
+    <span>
+      <a href={url}>{title}</a>
+    </span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
+  </li>
 );
 
 export default App;
