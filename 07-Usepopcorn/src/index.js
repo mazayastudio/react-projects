f925230d3d6b0b01a37f6741fd5f3b1747317dff
sayudha <mazaya.studio@outlook.com>
2025-02-02 17:15:26 +0700

    Enhance StarRating with customization options
    Added props for `color`, `size`, `messages`, and `defaultRating` to make the StarRating component more flexible and customizable. Adjusted styles dynamically based on these props and updated example usage in `index.js` to demonstrate the new features.

--- 07-Usepopcorn/src/index.js
+++ 07-Usepopcorn/src/index.js
@@ -8,7 +8,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}
+    />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={20}
+      defaultRating={3}
+      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}
+    />
   </React.StrictMode>,
 );
 
