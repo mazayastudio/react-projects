4d5c682b9565d5327a66951cfea72a0868659194
sayudha <mazaya.studio@outlook.com>
2025-02-02 14:10:40 +0700

    Add StarRating component and update index.js for usage
    Introduced a new StarRating component with customizable maxRating. Updated index.js to render StarRating examples instead of the previous App component. This change sets up a foundation for handling ratings in the application.

---
+++ 07-Usepopcorn/src/StarRating.js
@@ -0,0 +1,27 @@
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+};
+
+const startContainerStyle = {
+  display: 'flex',
+  gap    : '4px',
+};
+
+const textStyle = {
+  lineHeight: '1',
+  margin    : '0',
+};
+
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={startContainerStyle}>
+        {Array.from(
+          { length: maxRating }, (_, i) => <span key={i}>S{i + 1}</span>)}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+}
