9d2bb84c201af8fe0b4454ab8df6c8f89e8627b5
sayudha <mazaya.studio@outlook.com>
2025-02-02 14:36:34 +0700

    Add hover functionality to StarRating component
    Introduced temporary rating state to display star previews on hover. Updated the Star component to handle hover events and show real-time feedback. This enhances the user experience by providing visual cues before a rating is selected.

--- 07-Usepopcorn/src/StarRating.js
+++ 07-Usepopcorn/src/StarRating.js
@@ -17,6 +17,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   const handleRating = (rating) => {
     setRating(rating);
@@ -29,11 +30,13 @@ export default function StarRating({ maxRating = 5 }) {
           { length: maxRating }, (_, i) =>
             <Star
               key={i}
+              full={tempRating ? i + 1 <= tempRating : i + 1 <= rating}
               onRate={() => handleRating(i + 1)}
-              full={i + 1 <= rating}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
             />)}
       </div>
-      <p style={textStyle}>{rating || ''}</p>
+      <p style={textStyle}>{tempRating || rating || ''}</p>
     </div>
   );
 }
@@ -44,12 +47,14 @@ const starStyle = {
   display: 'block',
   cursor : 'pointer',
 };
-const Star = ({ onRate, full }) => {
+const Star = ({ onRate, onHoverIn, onHoverOut, full }) => {
   return (
     <span
       role="button"
       style={starStyle}
       onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
     >
       {full ? <svg
         xmlns="http://www.w3.org/2000/svg"
