e260813e4d67299dcafff7af4cb462710f102773
sayudha <mazaya.studio@outlook.com>
2025-02-04 06:36:08 +0700

    Refactor components and improve functionality.
    Replaced `StarRating` with `App` as the main component in `index.js` and added OMDB API data fetching to `App.js`. Enhanced `08-how-react-works` by implementing new handlers for incrementing likes and undo functionality.

--- 07-Usepopcorn/src/index.js
+++ 07-Usepopcorn/src/index.js
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+// import StarRating from './StarRating';
+import './index.css';
+import App from './App';
 
-const Test = () => {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating
-        color="blue"
-        maxRating={10}
-        onSetRating={setMovieRating}
-      />
-      <p>This moves was rated {movieRating} stars</p>
-    </div>
-  );
-};
+// const Test = () => {
+//   const [movieRating, setMovieRating] = useState(0);
+//
+//   return (
+//     <div>
+//       <StarRating
+//         color="blue"
+//         maxRating={10}
+//         onSetRating={setMovieRating}
+//       />
+//       <p>This moves was rated {movieRating} stars</p>
+//     </div>
+//   );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
-    <StarRating
-      maxRating={5}
-      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}
-    />
-    <StarRating
-      maxRating={5}
-      color="red"
-      size={20}
-      defaultRating={3}
-      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}
-    />
+    <App />
+    {/*<StarRating*/}
+    {/*  maxRating={5}*/}
+    {/*  messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}*/}
+    {/*/>*/}
+    {/*<StarRating*/}
+    {/*  maxRating={5}*/}
+    {/*  color="red"*/}
+    {/*  size={20}*/}
+    {/*  defaultRating={3}*/}
+    {/*  messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}*/}
+    {/*/>*/}
 
-    <Test />
+    {/*<Test />*/}
   </React.StrictMode>,
 );
 
